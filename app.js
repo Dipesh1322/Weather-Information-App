36bf08925c61647746afa673535609ca86cecb70
Dipesh Chaudhary <108289234+Dipesh1322@users.noreply.github.com>
2023-03-01 23:34:26 +0530

    Add 'feels like' info.

--- app.js
+++ app.js
@@ -23,16 +23,19 @@ app.post("/",function(req,res){
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+query+"&units="+unit+"&appid="+apiKey;
 
     https.get(url,function(response){
-        // console.log(response.statusCode);
+        console.log(response.statusCode);
         response.on("data",function(data){
             const weatherData = JSON.parse(data);
+            console.log(weatherData);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
+            const feelsLike = weatherData.main.feels_like;
             const icon = weatherData.weather[0].icon;
             const iconURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
 
             res.write("<h1>The temperature in " + query + " is " + temp + " degrees Celcius.</h1>");
             res.write("<img src=" + iconURL + ">");
+            res.write("<h2>Feels like "+feelsLike+" degrees Celcius.</h2>");
             res.send();
         })
     })
